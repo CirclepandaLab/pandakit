9e37cc2fa8fc87f4211c3f5135e9b7e8217395b5
CHAITANYA <Chaitanyas0101@gmail.com>
2020-02-17 08:20:47 +0530

    added 2nd slider.  (#47)
    * change spelling to bar
    
    * added 2nd slider

--- assets/js/pandakit.slider.js
+++ assets/js/pandakit.slider.js
@@ -4,3 +4,18 @@
  * @author    Precious Tom
  * @copyright Precious Tom. All rights reserved.
  */
+
+/*slider 2*/
+var myIndex = 0;
+carousel();
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("carouse2");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); 
+}

eae588fededcb2bf8fafe6e3f802193f422615fc
Precious Tom <tomprezine@gmail.com>
2020-02-21 23:44:25 +0100

    clear spaces, fix to-top script, and change filter tags to div

--- assets/js/pandakit.js
+++ assets/js/pandakit.js
@@ -4,17 +4,15 @@
 
 // Back to top
 let scrollbutton = document.getElementById("return-to-top");
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    scrollbutton.style.display = "block";
-  } else {
-    scrollbutton.style.display = "none";
-  }
-}
-
-// Smooth Scroll
 if (scrollbutton) {
+window.onscroll = function() {scrollFunction()};
+  function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      scrollbutton.style.display = "block";
+    } else {
+      scrollbutton.style.display = "none";
+    }
+  }
 	scrollbutton.onclick = function() {
 		window.scrollTo({top: 0, behavior: 'smooth'}); 
 	};

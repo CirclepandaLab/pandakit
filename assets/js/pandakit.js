895dc0eb897a0d74b9b51f7429f54ecb0fc176c7
Chaitanya <chaitanyas0101@gmail.com>
2020-03-03 23:16:04 +0530

    added to-do list

--- assets/js/pandakit.js
+++ assets/js/pandakit.js
@@ -53,3 +53,20 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 document.getElementById("close").onclick = function() {openNav()};
+
+//to-do list
+//item addition script
+function todo(){
+  var item  = document.getElementById('todoinput').value
+  var text = document.createTextNode(item)
+  var newItem = document.createElement("li")
+  newItem.appendChild(text)
+  document.getElementById("todolist").appendChild(newItem)
+}
+document.getElementById("click").onclick = function() {todo()};
+//item deletion script
+function del(){
+  var item1 = document.getElementById('todolist');
+  item1.removeChild(item1.childNodes[0]);
+}
+document.getElementById("cross").onclick = function() {del()};
